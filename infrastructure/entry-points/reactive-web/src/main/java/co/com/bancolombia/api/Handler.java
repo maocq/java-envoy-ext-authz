9b2410096d7a6b8da35903e2a6cf108410e7ac3e
maocq <johncarm@bancolombia.com.co>
2023-11-17 14:07:31 -0500

    jwt

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,23 +1,37 @@
 package co.com.bancolombia.api;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Component
 @RequiredArgsConstructor
 public class Handler {
 
     public Mono<ServerResponse> listenUseCase(ServerRequest serverRequest) {
-        return serverRequest.bodyToMono(String.class)
-                .switchIfEmpty(Mono.just("Empty"))
-                .doOnNext(body -> printInfo(serverRequest, body))
-                .flatMap(response -> ServerResponse.ok().bodyValue(response));
+        printInfo(serverRequest);
+
+        if (serverRequest.headers().firstHeader("x-value") == null)
+            return ServerResponse.status(FORBIDDEN).bodyValue(Response.builder().message("Invalid authorization :(").build());
+
+        return serverRequest.principal()
+                .flatMap(principal -> {
+                    if (principal instanceof Authentication authentication)
+                        System.out.println("Authorities: " + authentication.getAuthorities());
+
+                    return ServerResponse.ok()
+                            .header("x-user-name", principal.getName())
+                            .bodyValue(Response.builder().message(principal.getName()).build());
+                });
     }
 
-    private void printInfo(ServerRequest serverRequest, String body) {
+    private void printInfo(ServerRequest serverRequest) {
         System.out.println("Method: " + serverRequest.method());
         System.out.println("Path: " + serverRequest.path());
         System.out.println("Uri: " + serverRequest.uri());
@@ -26,7 +40,8 @@ public class Handler {
 
         System.out.println("Path variables: " + serverRequest.pathVariables());
         System.out.println("Query params: " + serverRequest.queryParams());
-
-        System.out.println("Body: " + body);
     }
+
+    @Builder
+    record Response(String message) {}
 }
